58818dc96bdfe330d1202a40ac7e5fa81dbc564b
seanat0r <seanatorhd@gmail.com>
2024-07-17 16:21:50 +0200

    finished the countdown

--- script.js
+++ script.js
@@ -1,112 +1,65 @@
 const dateCalculation = () => {
-    let submitForm = document.getElementById('submit');
-    const currentDate = () => {
-        const now = new Date();
+  let submitForm = document.getElementById('submit');
 
-        let day = now.getDate();
-        let month = now.getMonth() + 1;
-        let year = now.getFullYear();
+  const currentDate = () => {
+    const now = new Date();
 
-        if (day < 10) {
-            day = '0' + day;
-        };
-        if (month < 10) {
-            month = '0' + month;
-        };
+    let day = now.getDate();
+    let month = now.getMonth() + 1;
+    let year = now.getFullYear();
 
-        return `${year}-${month}-${day}`;
+    if (day < 10) {
+      day = '0' + day;
     }
-    submitForm.addEventListener(('submit'), (e) => {
-        e.preventDefault();
+    if (month < 10) {
+      month = '0' + month;
+    }
 
-        let targetDate = document.getElementById('targetDate').value; 
-        let currentYear = '0';
-        let currentDateMMDD = '0';
+    return `${year}-${month}-${day}`;
+  };
 
-        let targetYear = '0';
-        let targetDateMMDD = '0';  
+  submitForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-        let currentDateDD = '0';
-        let targetDateDD = '0';
-        
-        let currentDateYYYYNumber = 0;
-        let currentDateMMDDNumber = 0;
-        let currentDDNumber = 0;
-
-        let targetDateYYYYNumber = 0;
-        let targetDateMMDDNumber = 0;
-        let targetDateDDNumber = 0;
-        
-        const formatingTheDate = (currentDate) => {
-            /* Getting each YYYY/MM/DD as a sepreat variabel */
-            const sliceYearCurrent = (currentDate) => {
-                currentYear = currentDate.slice(0,4);
-                currentDateMMDD = currentDate.slice(4);
-                console.log('YYYYMMDD: ' + currentYear + ',' + currentDateMMDD);
-            };
-            const sliceYearTarget = (targetDate) => {
-                targetYear = targetDate.slice(0,4);
-                targetDateMMDD = targetDate.slice(4);
-                console.log (targetYear + ',' +targetDateMMDD);
-            };
-
-            sliceYearCurrent(currentDate);
-            sliceYearTarget(targetDate);
-
-            const sliceMonthCurrent = (currentDateMMDD) => {
-                currentDateMMDD = currentDateMMDD.slice(0,2);
-                currentDateDD = currentDateMMDD.slice(2);
-                console.log ('MMDD: ' + currentDateMMDD + currentDateDD);
-            };
-            const sliceMonthTarget = (targetDateMMDD) => {
-                targetDateMMDD = currentDateMMDD.slice(0,2);
-                targetDateDD = currentDateMMDD.slice(2);
-                console.log ('MMDD: ' + targetDateMMDD + targetDateDD);
-            };
-            
-            sliceMonthCurrent(currentDateMMDD);
-            sliceMonthTarget(targetDateMMDD);
-
-            const changeDatesToInt = (currentDate, currentDateMMDD, currentDateDD, targetDate, targetDateMMDD, targetDateDD) => {
-            let currentDateYYYYNumber = parseInt(currentDate);
-            let currentDateMMDDNumber = parseInt(currentDateMMDD); 
-            let currentDDNumber = parseInt(currentDateDD); 
-            let targetDateYYYYNumber = parseInt(targetDate);
-            let targetDateMMDDNumber = parseInt(targetDateMMDD);
-            let targetDateDDNumber = parseInt(targetDateDD);
-            }
-
-            const calculationTargetCurrent = (currentDateYYYYNumber, currentDateMMDDNumber, currentDateDD, targetDateYYYYNumber, targetDateMMDDNumber, targetDateDDNumber) => {
-                /* Checks if the year is a leapYear and when not how many years left until the next leapyear*/
-                let nextLeapYear = 4;
-                let leapYear = true;
-                if ((currentDateYYYYNumber % 4 === 0 && currentDateYYYYNumber % 100 !== 0) && (currentDateYYYYNumber % 400 !== 0)) {
-                    let howManyYearsUntilLeapYear = currentDateYYYYNumber / 4;
-                    if (howManyYearsUntilLeapYear % 1 === 0.25) {
-                        nextLeapYear = 3;
-                        leapYear = false;
-                    } else if (howManyYearsUntilLeapYear % 1 === 0.5) {
-                        nextLeapYear = 2;
-                        leapYear = false; 
-                    } else {
-                        nextLeapYear = 1;
-                        leapYear = false;
-                    };
-                };
-                /* Checks if current and target the same year is or not */
-                if (!currentDateYYYYNumber === targetDateYYYYNumber) {
-                    
-                }
-            };
-            changeDatesToInt (currentDate, currentDateMMDD, currentDateDD, targetDate, targetDateMMDD, targetDateDD)
-            calculationTargetCurrent(currentDateYYYYNumber, currentDateMMDDNumber, currentDateDD, targetDateYYYYNumber, targetDateMMDDNumber, targetDateDDNumber)
-            
+    const formatingTheDate = () => {
+      const getTargetDate = () => {
+        let targetDateValue = document.getElementById('targetDate').value;
+        if (targetDateValue) {
+          let targetDateHolder = new Date(targetDateValue);
+          let targetTimeStamp = targetDateHolder.getTime();
+          return targetTimeStamp;
+        } else {
+          console.log('targetDateValue is not true');
         }
-        formatingTheDate(currentDate, targetDate);
-    })
-    console.log(currentDate());
-    
-};
+      };
+      let targetDate = getTargetDate();
+      //let targetDate = new Date("Jan 5, 2030 15:37:25").getTime();
 
+      const counter = setInterval(function () {
+        const startDate = Date.now();
+
+        let distance = targetDate - startDate;
+
+        //Calculate the The days, hours, minutes and second
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        //display the timer on the web
+        document.getElementById('CountdownCloack').innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        if(distance < 0) {
+          clearInterval(counter);
+          document.getElementById('CountdownCloack').innerHTML = 'EXPIRED';
+        }
+      }, 1000);
+    };
+    formatingTheDate();
+  });
+};
 
 dateCalculation();
--- test.js
+++ test.js
@@ -1,166 +1,23 @@
-let dateToday;
+const dateCalculation = () => {
+  //let targetDate = document.getElementById('targetDate').value.getTime;
+  let targetDate = new Date("Jan 5, 2030 15:37:25").getTime();
 
-const currentDate = () => {
-  const now = new Date();
+  const counter = setInterval(0 = () => {
+    const startDate = Date.now();
 
-  let day = now.getDate();
-  let month = now.getMonth() + 1;
-  let year = now.getFullYear();
+    let distance = targetDate - startDate;
 
-  if (day < 10) {
-    day = '0' + day;
-  }
-  if (month < 10) {
-    month = '0' + month;
-  }
+    //Calculate the The days, hours, minutes and second
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  dateToday = `${year}-${month}-${day}`;
-  return dateToday;
+    //display the timer on the web
+    
+  })
+
 };
 
-const formatingTheDate = () => {
-  let currentYear;
-  let currentMonth;
-  let currentDateDD;
-  let targetYear;
-  let targetMonth;
-  let targetDateDD;
-  let targetDate = '2032-06-25';
-  let currentDateMMDD;
-  let targetDateMMDD;
-  let currentYearNumber;
-  let currentMonthNumber;
-  let currentDateDDNumber;
-  let targetYearNumber;
-  let targetMonthNumber;
-  let targetDateDDNumber;
+dateCalculation();
 
-  let daysCount;
-
-  const sliceYearCurrent = () => {
-    currentYear = dateToday.slice(0, 4);
-    currentDateMMDD = dateToday.slice(5);
-    console.log('YYYY-MM-DD(current): ' + currentYear + ',' + currentDateMMDD);
-  };
-
-  const sliceYearTarget = (targetDate) => {
-    targetYear = targetDate.slice(0, 4);
-    targetDateMMDD = targetDate.slice(5);
-    console.log('YYYY-MM-DD(target): ' + targetYear + ',' + targetDateMMDD);
-  };
-
-  const sliceMonthCurrent = () => {
-    currentMonth = currentDateMMDD.slice(0, 2);
-    currentDateDD = currentDateMMDD.slice(3);
-    console.log('MM-DD(current): ' + currentMonth + ',' + currentDateDD);
-  };
-
-  const sliceMonthTarget = () => {
-    targetMonth = targetDateMMDD.slice(0, 2);
-    targetDateDD = targetDateMMDD.slice(3);
-    console.log('MM-DD(target): ' + targetMonth + ',' + targetDateDD);
-  };
-
-  sliceYearCurrent(currentDate);
-  sliceYearTarget(targetDate);
-
-  sliceMonthCurrent();
-  sliceMonthTarget();
-
-  const changeDatesToInt = (
-    currentYear,
-    currentMonth,
-    currentDateDD,
-    targetYear,
-    targetMonth,
-    targetDateDD
-  ) => {
-    currentYearNumber = parseInt(currentYear);
-    currentMonthNumber = parseInt(currentMonth);
-    currentDateDDNumber = parseInt(currentDateDD);
-    targetYearNumber = parseInt(targetYear);
-    targetMonthNumber = parseInt(targetMonth);
-    targetDateDDNumber = parseInt(targetDateDD);
-  };
-
-  const calculationTargetCurrent = (
-    currentYearNumber,
-    currentMonthNumber,
-    currentDateDDNumber,
-    targetYearNumber,
-    targetMonthNumber,
-    targetDateDDNumber
-  ) => {
-    /* Add the days to the next full year */
-    /* Checks if current year a leap year is */
-    let isLeapYear;
-    if (
-      (currentYearNumber % 4 === 0 && currentYearNumber % 100 !== 0) ||
-      currentYearNumber % 400 === 0
-    ) {
-      isLeapYear = true;
-    } else {
-      isLeapYear = false;
-    }
-    const monthInDays = [
-      31,
-      28 + (isLeapYear ? 1 : 0),
-      31,
-      30,
-      31,
-      30,
-      31,
-      31,
-      30,
-      31,
-      30,
-      31,
-    ];
-    /* Added the the days to the next month */
-    let indexStartMonth = currentMonthNumber - 1;
-    let daysOfcurrentMonth = monthInDays[indexStartMonth];
-    daysCount = daysOfcurrentMonth - currentDateDDNumber;
-
-    /* Added the days to the next year */
-    indexStartMonth += 1;
-    for (; indexStartMonth < monthInDays.length; indexStartMonth++) {
-      daysCount += monthInDays[indexStartMonth];
-    }
-    console.log(daysCount);
-
-    /* Add the days to the target year */
-    currentYearNumber += 1;
-    console.log('CYN: ' + currentYearNumber + ' TYN: ' + targetYearNumber);
-    if (currentYearNumber > targetYearNumber) {
-      for (; currentYearNumber <= targetYearNumber - 1; currentYearNumber++) {
-        if (
-          (currentYearNumber % 4 === 0 && currentYearNumber % 100 !== 0) ||
-          currentYearNumber % 400 === 0
-        ) {
-          daysCount += 365;
-        } else {
-          daysCount += 366;
-        }
-      }
-    }
-  };
-  changeDatesToInt(
-    currentYear,
-    currentMonth,
-    currentDateDD,
-    targetYear,
-    targetMonth,
-    targetDateDD
-  );
-  calculationTargetCurrent(
-    currentYearNumber,
-    currentMonthNumber,
-    currentDateDDNumber,
-    targetYearNumber,
-    targetMonthNumber,
-    targetDateDDNumber
-  );
-};
-
-currentDate();
-formatingTheDate();
